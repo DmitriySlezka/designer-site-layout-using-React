95ddf0a7110fff903600fd3a6b6ab0b9857beacf
DmitriySlezka <dimaslezka@gmail.com>
2020-06-06 16:47:03 +0300

    create build

---
+++ build/precache-manifest.88e6bd182ada05d5b486edccb03c5974.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50bdffd6c2321fb97e5f47f65006424c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dc98f6d290fae187c693",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "566c57cc063324915430",
+    "url": "/static/css/main.c171151c.chunk.css"
+  },
+  {
+    "revision": "dc98f6d290fae187c693",
+    "url": "/static/js/2.d72e1756.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.d72e1756.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "566c57cc063324915430",
+    "url": "/static/js/main.15c28843.chunk.js"
+  },
+  {
+    "revision": "fa82f6da889805e7b3e1",
+    "url": "/static/js/runtime-main.7d54ab6f.js"
+  },
+  {
+    "revision": "1a51c82f5e75c3f844e234ee30693b9d",
+    "url": "/static/media/girl-photo.1a51c82f.jpeg"
+  },
+  {
+    "revision": "50117d98a24d343ea62f49388479d434",
+    "url": "/static/media/project1.50117d98.jpg"
+  },
+  {
+    "revision": "1d992a2cb1225e3028107c9c0ef88f19",
+    "url": "/static/media/project2.1d992a2c.jpg"
+  },
+  {
+    "revision": "2d213830e71638f8c7ddfc0cf4900f92",
+    "url": "/static/media/project3.2d213830.png"
+  },
+  {
+    "revision": "c4936093cb2a42030cb540f3af905ef0",
+    "url": "/static/media/project4.c4936093.png"
+  },
+  {
+    "revision": "a155275268a56f63a4bfd3693a7a5830",
+    "url": "/static/media/signature.a1552752.png"
+  }
+]);
